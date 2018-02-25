e640717a36db0f0d818597271f7c8579e724f4f0
silviumacovei <smacovei@007>
2018-02-25 20:24:48 +0200

    change name from ListItem to AdministrationItem

--- app/src/main/java/com/macovei/silviu/economictime/data/entity/AdministrationItem.kt
+++ app/src/main/java/com/macovei/silviu/economictime/data/entity/AdministrationItem.kt
@@ -7,17 +7,17 @@ import android.arch.persistence.room.PrimaryKey
 /**
  * Created by silviumacovei on 2/21/18.
  */
-@Entity(tableName = "items")
-data class ListItem(
+@Entity(tableName = "AdministrationItems")
+data class AdministrationItem(
         @PrimaryKey(autoGenerate = true)
         var uid: Long? = 0,
         @field:ColumnInfo(name = "date")
-        var date: String,
+        var date: String = "",
         @field:ColumnInfo(name = "project")
-        var project: String,
+        var project: String = "",
         @field:ColumnInfo(name = "activity")
-        var activity: String,
+        var activity: String = "",
         @field:ColumnInfo(name = "hours")
-        var hours: String,
+        var hours: Int = 0,
         @field:ColumnInfo(name = "status")
-        var status: String)
+        var status: String = "")
